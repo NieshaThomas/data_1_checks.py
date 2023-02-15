547d1b3b0f08c49f437e7c36e1b5b9554416216d
niesha Thomas <shedeserves.75@gmail.com>
2023-02-15 13:05:45 -0500

    first commit

--- knowlegecheck1.py
+++ knowlegecheck1.py
@@ -2,28 +2,17 @@
 print('Hello World!')
 
 #a list populated with several values. They can be strings / integers / floats etc. Then, print() one of those values either to the prompt
-person = 'knowledge Check'
-value_1 = '0219'
-value_2 = '23'
-float_value = 12.0
-print(float_value)
-print(value_1)
-print(person)
-print(value_1 + value_2)
+my_list = 'knowledge Check', ' 0.2', '1923'
+print(my_list)
 
 # dictionary populated with two keys and two values. 
-Dict = {1: 'Love', 2: 'My', 3: 'Family'}
-print(Dict)
-print({1:'Love'})
-print({2:'My'})
-print({3:'Family'})
+my_dict = {1: 'Love', 2: 'My', 3: 'Family'}
+print(my_dict.get(3))
 
 
 #change tuple value
 my_tuple = ('my', 'first', 'knowledge', 'test')
-a=list(my_tuple)
-a[2] = 'passed'
-my_tuple = tuple(a)
-print(my_tuple)
+print(my_tuple[1], my_tuple[3])
+
 
 
