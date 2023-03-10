64d2f366cdf3ece5a45092705052679191bb3fa5
Niesha.Thomas <shedeserves.75@gmail.com>
2023-03-10 18:25:20 -0500

    added file to commit

---
+++ kc_2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import csv
+
+import matplotlib.pyplot as plt
+
+import csv
+
+with open("C:Users/shede/OneDrive/Desktop/git/data_1_checks.py/ASSETSLouisville_Metro_KY_-_LMPD_Hate_Crimes - Copy.csv, 'r'") as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv.reader:
+        print(line)
+
+
+
+
+
+
+
