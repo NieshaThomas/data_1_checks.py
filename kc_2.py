de6a2575086d9136ca9072bf79adcd582aa6d9e9
Niesha.Thomas <shedeserves.75@gmail.com>
2023-03-11 18:53:18 -0500

    added kc_2plot

--- kc_2.py
+++ kc_2.py
@@ -5,11 +5,22 @@ import matplotlib.pyplot as plt
 
 import csv
 
-with open("C:Users/shede/OneDrive/Desktop/git/data_1_checks.py/ASSETSLouisville_Metro_KY_-_LMPD_Hate_Crimes - Copy.csv, 'r'") as csv_file:
-    csv_reader = csv.reader(csv_file)
+import numpy as np
 
-    for line in csv.reader:
-        print(line)
+data = pd.read_csv(r'C:\Users\shede\OneDrive\Desktop\git\data_1_checks.py\ASSETS\Louisville_Metro_KY_-_LMPD_Hate_Crimes.csv')
+df = pd.DataFrame(data, columns=['CRIME_TYPE','BIAS_MOTIVATION_GROUP', 'ZIP_CODE', 'PREMISE_TYPE'])
+
+print(df)
+
+labels = 'ANTI-BLACK', 'ANTI-WHITE', 'ANTI-MALE HOMOSEXUAL (GAY)', 'ANTI-FEMALE HOMOSEXUAL (LESBIAN)'
+sizes = [15, 30, 45, 10]
+
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels, autopct= '%1.1f%%')
+
+plt.show()
+
+
 
 
 
